f9fec6177dd5eed997a77d5d57011cd79c590dd4
Mohamed Shahbain <shahbain213@gmail.com>
2026-05-17 18:03:25 -0400

    Initial commit: empty Xcode project

---
+++ YearbookApp/YearbookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YearbookAppApp.swift
+//  YearbookApp
+//
+//  Created by Mohamed Shahbain on 5/17/26.
+//
+
+import SwiftUI
+
+@main
+struct YearbookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
